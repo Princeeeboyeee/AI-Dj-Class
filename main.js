66c38a55e713899b0adb5e288efefb74d2012309
Princeeeboyeee <88536083+Princeeeboyeee@users.noreply.github.com>
2022-08-03 19:14:26 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+song="";
+
+function preload(){
+    song=loadSound("Cradles.mp3");
+}
+
+function setup(){
+canvas=createCanvas(800,600);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+
+function draw(){
+    image(video, 0,0, 800, 600);
+}
+
+function play(){
+    song.setVolume(1);
+    song.play();
+    console.log("Song has started playing");
+}
