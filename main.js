a25f0cfb0562acb4f0274baa785a2f60dfe9543d
Princeeeboyeee <88536083+Princeeeboyeee@users.noreply.github.com>
2022-09-21 19:12:33 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ leftWristY=0;
 rightWristX=0;
 rightWristY=0;
 scoreleftwrist=0;
+scorerightwrist=0;
 
 
 function preload(){
@@ -30,7 +31,41 @@ function draw(){
     remove_decimals=floor(InNumberleftWristY);
     volume=remove_decimals/500;
     document.getElementById("volume").innerHTML= 'Volume : ' +  volume;
-    //song.setVolume(volume);
+    song.setVolume(volume);
+    }
+    
+
+    if(scorerightwrist>0.2){
+
+    circle(rightWristX, rightWristY, 40);
+    fill("#00FFFF");
+    stroke("#32CD32");
+
+    if(rightWristY>0 && rightWristY<=100){
+    document.getElementById("speed").innerHTML= 'Speed = 2x';
+    song.rate(2);
+    }
+
+    else if(rightWristY>100 && rightWristY<=200){
+        document.getElementById("speed").innerHTML= 'Speed = 1.5x';
+        song.rate(1.5);
+        }
+
+    else if(rightWristY>200 && rightWristY<=300){
+            document.getElementById("speed").innerHTML= 'Speed = 1x';
+            song.rate(1);
+            }
+
+    else if(rightWristY>300 && rightWristY<=400){
+                document.getElementById("speed").innerHTML= 'Speed = 0.5x';
+                song.rate(0.5);
+                }
+
+    else if(rightWristY>400 && rightWristY<=500){
+                    document.getElementById("speed").innerHTML= 'Speed = 0.25x';
+                    song.rate(0.25);
+                    }
+
 }
 }
 
@@ -54,6 +89,10 @@ function gotPoses(results)
 
         scoreleftwrist=results[0].pose.keypoints[9].score;
         console.log('Score Left Wrist: ' + scoreleftwrist);
+
+        scorerightwrist=results[0].pose.keypoints[10].score;
+        console.log('Score Right Wrist: ' + scorerightwrist);
+
 
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
